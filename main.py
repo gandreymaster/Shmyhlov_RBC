661668cdfa480c5a9b991bde72b3889b3512fdcf
gandreymaster <75809086+gandreymaster@users.noreply.github.com>
2024-05-18 18:07:41 +0300

    Add files via upload
    "A tutorial to set safety stock under guaranteed-service time by dynamic programming" implementation

---
+++ main.py
@@ -0,0 +1,150 @@
+import sc
+import numpy as np
+import pandas as pd
+
+I = 6
+D = 6
+my = 120
+sigma = 40
+sl = 95
+z = 1.64
+h = 0.2
+theta = 0
+
+cost = np.array([3, 4, 1, 2, 3, 1])
+time = np.array([4, 1, 6, 5, 5, 2])
+cumulative_cost = np.array([3, 4, 8, 2, 13, 14])
+max_replenish = np.array([4, 1, 10, 5, 15, 17])
+std_devs = np.array([40, 40, 40, 40, 40, 40])
+
+node_list = np.array(range(1, I + 1))
+D_lookup = {idx + 1: val for idx, val in enumerate(np.isin(node_list, D).tolist())}
+
+L = {(1, 3), (2, 3), (3, 5), (4, 5), (5, 6)}
+U = [(1, 1), (2, 2), (3, 4), (4, 6), (5, 5), (6, 3)]
+
+###### forward pass #######
+
+L = set(L)
+
+result_array = dict()
+result_array_lamb = dict()
+result_array_phi = dict()
+
+final_lambdas = dict()
+final_phis = dict()
+
+U_labeled_first_value_array = np.array([x for x, y in U])
+U_labeled_second_value_array = np.array([y for x, y in U])
+L_first_value_array = np.array([x for x, y in L])
+L_second_value_array = np.array([y for x, y in L])
+L_array = np.array(list(L))
+
+for li in U:
+
+    stage = li[1]
+    it = stage - 1
+
+    stream = sc.get_adjacent_nodes(L, li, U, U_labeled_first_value_array, U_labeled_second_value_array, L_array)
+
+    x = max_replenish[it] - time[it] + 1
+    y = max_replenish[it] + 1
+
+    if D_lookup[stage]:
+        y = 1
+
+    assert x > 0
+
+    array = np.zeros((x, y))
+
+    for lam in range(x):
+        for phi in range(y):
+            array[lam, phi] = h * cumulative_cost[it] * z * np.nan_to_num(std_devs[it]) * np.sqrt(
+                lam + time[it] - phi)
+            for t in stream.keys():
+                if stream[t] == 'down':
+                    array[lam, phi] += np.nanmin(result_array_lamb[t][phi:y]) if result_array_lamb[t][
+                                                                                 phi:y].size > 0 else 0
+                elif stream[t] == 'up':
+                    array[lam, phi] += np.nanmin(result_array_phi[t][:lam + 1])
+
+    if D_lookup[stage]:
+        lamb_cap = array.copy().flatten()
+        phi_cap = np.nanmin(array, axis=1).flatten()
+    else:
+        lamb_cap = np.nanmin(array, axis=1).flatten()
+        phi_cap = array.copy().flatten()
+
+    phi_cap = np.zeros((1, y)).flatten()
+    for phi in range(y):
+        phi_cap[phi] = np.nanmin(array[max(0, phi - time[it]):, phi])
+
+    result_array[stage] = array.copy()
+    result_array_lamb[stage] = lamb_cap
+    result_array_phi[stage] = phi_cap
+
+    final_phis[stage] = result_array_phi[stage].argmin()
+    final_lambdas[stage] = result_array_lamb[stage].argmin()
+
+L = list(L)
+
+for l in range(I - 1, 0, -1):
+
+    label, stage = U[l - 1]
+    it = stage - 1
+
+    indices = np.where(L_second_value_array == stage)[0].tolist()
+
+    if len(indices) > 0:
+
+        origins = [L[i][0] for i in indices]
+
+        ii1 = np.where(np.isin(U_labeled_second_value_array, origins))[0]
+        origin_labels = [U[i][0] for i in ii1]
+        correct_origins = [origins[i] for i in np.where(np.less(label, origin_labels) == True)[0]]
+
+        for stage_to_be_processed in correct_origins:
+            final_lambdas[stage] = final_phis[stage_to_be_processed]
+            lambdaa = final_lambdas[stage]
+
+            phii = np.nanargmin(result_array[stage][lambdaa, :]) if result_array[stage].shape[1] > lambdaa else 0
+
+            final_phis[stage] = phii
+    else:
+
+        indices = np.where(L_first_value_array == stage)[0].tolist()
+
+        destinations = [L[i][1] for i in indices]
+
+        ii1 = np.where(np.isin(U_labeled_second_value_array, destinations))[0]
+        destination_labels = [U[i][0] for i in ii1]
+        correct_destinations = [destinations[i] for i in np.where(np.less(label, destination_labels) == True)[0]]
+
+        for stage_to_be_processed in correct_destinations:
+
+            if max_replenish[it] > final_lambdas[stage_to_be_processed]:
+                final_phis[stage] = final_lambdas[stage_to_be_processed]
+
+            lambdaa = np.nanargmin(result_array[stage][:, final_phis[stage]])
+
+            final_lambdas[stage] = lambdaa
+
+idx_list, lambda_list, phi_list, days_inv_list, ss_cost_list, ss_list = [], [], [], [], [], []
+
+for a, b in zip(final_lambdas.items(), final_phis.items()):
+    idx_list.append(a[0])
+    lambda_list.append(int(a[1]))
+    phi_list.append(b[1])
+    days_inv_list.append(max(int(a[1]) + time[a[0] - 1] - b[1], 0))
+    ss_cost = h * cumulative_cost[a[0] - 1] * z * std_devs[a[0] - 1] * np.sqrt(
+        max(int(a[1]) + time[a[0] - 1] - b[1], 0))
+    ss_cost_list.append(ss_cost)
+    ss_list.append(ss_cost / (h * cumulative_cost[a[0] - 1]))
+
+di_results = {'Index': idx_list, 'Lambda': lambda_list, 'Phi': phi_list, 'Days_of_inventory': days_inv_list,
+              'Safety_stock_cost': ss_cost_list, 'Safety_stock': ss_list}
+
+df_results = pd.DataFrame(di_results)
+df_results = df_results.sort_values('Index')
+print(df_results)
+print(f"Total safety stock cost = {sum(df_results['Safety_stock_cost'])}")
---
+++ sc.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+
+def get_adjacent_nodes(set_links, link, U_labeled, U_labeled_first_value_array,
+                       U_labeled_second_value_array, L_array):
+    stage = link[1]
+    label = link[0]
+
+    k, l = np.where(L_array == stage)
+
+    inter_result = set().union(*[set_links & {(stage, j), (j, stage)} for j in
+                                 np.unique(np.array([list(set_links)[i] for i in k]).flatten()).tolist() if stage != j])
+
+    li_collect = list(inter_result)
+
+    di_map1 = {a[0]: 'up' for a in li_collect if a[1] == stage}
+    di_map2 = {a[1]: 'down' for a in li_collect if a[0] == stage}
+
+    di_mapping = {**di_map1, **di_map2}
+
+    candidates = [k for k in di_mapping.keys() if k != stage]
+    candidate_labels = [x for x in
+                        U_labeled_first_value_array[np.isin(U_labeled_second_value_array, candidates)].tolist() if
+                        x < label]
+
+    result_stages = [U_labeled[i - 1][1] for i in candidate_labels]
+
+    return {k: v for k, v in di_mapping.items() if k in result_stages}
+
+
+def get_next_nodes(array_links, array_links_first, array_links_second, ix1, ix2, stages):
+    if stages.size == 1:
+        return stages[0], ix1, ix2
+
+    for j in stages:
+
+        x1 = np.where(np.take(array_links_first, np.where(ix1)[0]) == j)[0]
+
+        if x1.size > 0:
+
+            list_all_stages_first = array_links[np.take(np.where(ix1)[0], x1)]
+            list_all_stages_first = list_all_stages_first.flatten()
+
+        else:
+            list_all_stages_first = list()
+
+        x2 = np.where(np.take(array_links_second, np.where(ix2)[0]) == j)[0]
+
+        if x2.size > 0:
+
+            list_all_stages_second = array_links[np.take(np.where(ix2)[0], x2)]
+            list_all_stages_second = list_all_stages_second.flatten()
+
+        else:
+            list_all_stages_second = list()
+
+        list_all_stages = np.concatenate((list_all_stages_first, list_all_stages_second))
+
+        index1 = np.where(list_all_stages == j)
+        list_all_connected_stages = np.delete(list_all_stages, index1)
+
+        list_connected_unvisited_stages = list_all_connected_stages[np.isin(list_all_connected_stages, stages)]
+
+        if list_connected_unvisited_stages.size == 1:
+            ix1 = np.isin(array_links_first, j)
+            ix2 = np.isin(array_links_second, j)
+
+            return j, ix1, ix2
+        else:
+            return j * -1, ix1, ix2
+
+
+def label_stages(I, L):
+    I = I
+    L = L
+
+    L_array = np.array(list(L))
+    L_first_value_array = np.array([x for x, y in L])
+    L_second_value_array = np.array([y for x, y in L])
+
+    N = list(range(1, I + 1))
+    U = list()
+    l = 1
+
+    N = np.array(list(reversed(sorted(N))))
+
+    gix1, gix2 = np.zeros(L_first_value_array.size, dtype=bool), np.zeros(L_first_value_array.size, dtype=bool)
+
+    while N.size > 0:
+
+        x, ix1, ix2 = get_next_nodes(L_array, L_first_value_array, L_second_value_array, ~gix1, ~gix2, N)
+
+        gix1 = np.logical_or(gix1, ix1)
+        gix2 = np.logical_or(gix2, ix2)
+
+        if x > 0:
+            U.append((l, x))
+            idx = np.where(N == x)
+            N = np.delete(N, idx)
+            l += 1
+        else:
+            x *= -1
+            idx = np.where(N == x)
+            N = np.delete(N, idx)
+            N = np.append(N, x)
+
+    return U
